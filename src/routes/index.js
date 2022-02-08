e1a53521e1e344ea6d298dbaeeff66b73221c1e3
Ezegrigolatto <ezegrigolatto@gmail.com>
2022-02-08 19:47:46 -0300

    downgrade node

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,34 +8,29 @@ const upload = multer();
 
 const router = Router();
 
-// router.post("/video", upload.single("file"), async (req, res) => {
-    router.post("/video", async (req, res) => {
-//     console.log("hasta aca llegue antes de guardar el video")
-//     console.log(req.file)
-//   try {
-//     const arch = req.file;
-//     await pipeline(
-//       arch.stream,
-//       fs.createWriteStream(`../uploads/${arch.originalName}`)
-//     );
-//     await converter.convertToThumbnail(
-//       `../uploads/${arch.originalName}`
-//     );
-//     const dir = fs.realpathSync(`../../thumbnail/`) + "/tn.png";
-//     res.send("ok");
-//   } catch (err) {
-//     console.log(err);
-//     res.send(err);
-//   }
-
-res.send("ok")
+router.post("/video", upload.single("file"), async (req, res) => {
+  try {
+    const arch = req.file;
+    await pipeline(
+      arch.stream,
+      fs.createWriteStream(`${__dirname}/../uploads/${arch.originalName}`)
+    );
+    await converter.convertToThumbnail(
+      `${__dirname}/../uploads/${arch.originalName}`
+    );
+    const dir = fs.realpathSync(`${__dirname}/../../thumbnail/`) + "/tn.png";
+    res.send("ok");
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
 });
-router.post("/thumbnail/tn.png", function (req, res) {
+router.post("/thumbnail", function (req, res) {
   console.log(req.body);
   res.send("ok");
 });
 
-router.get("/thumbnail/tn.png", (req, res) => {
+router.get("/thumbnail", (req, res) => {
   const dir = fs.realpathSync(`${__dirname}/../../thumbnail/`) + "/tn.png";
   res.sendFile(dir);
 });
