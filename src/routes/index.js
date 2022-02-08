756e9b68960e7eb89d32735a7e7f95361d63598d
Ezegrigolatto <ezegrigolatto@gmail.com>
2022-02-08 14:50:07 -0300

    first

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,27 +1,28 @@
 const { Router } = require("express");
-const converter = require('video-gif-thumbnail-generator')
-const fs = require('fs');
-const { promisify } = require('util');
+const converter = require("video-gif-thumbnail-generator");
+const fs = require("fs");
+const { promisify } = require("util");
 const pipeline = promisify(require("stream").pipeline);
 const multer = require("multer");
 const upload = multer();
 
-
 const router = Router();
 
-
 router.post("/video", upload.single("file"), async (req, res) => {
-    // const arch = req.file;
-    // await pipeline(arch.stream, fs.createWriteStream(`${__dirname}/../uploads/${arch.originalName}`));
-    // await converter.convertToThumbnail(`${__dirname}/../uploads/${arch.originalName}`);  
-    // const dir=fs.realpathSync(`${__dirname}/../../thumbnail/`) + "/tn.png";
-    res.send("ok");
+  // const arch = req.file;
+  // await pipeline(arch.stream, fs.createWriteStream(`${__dirname}/../uploads/${arch.originalName}`));
+  // await converter.convertToThumbnail(`${__dirname}/../uploads/${arch.originalName}`);
+  // const dir=fs.realpathSync(`${__dirname}/../../thumbnail/`) + "/tn.png";
+  res.send("ok");
+});
+router.post("/thumbnail", function (req, res) {
+  console.log(req.body);
+  res.send("ok");
 });
 
-router.get('/thumbnail', (req, res) => {
-    const dir=fs.realpathSync(`${__dirname}/../../thumbnail/`) + "/tn.png";
-    res.sendFile(dir);
-})
-
+router.get("/thumbnail", (req, res) => {
+  const dir = fs.realpathSync(`${__dirname}/../../thumbnail/`) + "/tn.png";
+  res.sendFile(dir);
+});
 
 module.exports = router;
