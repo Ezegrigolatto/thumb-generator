4498dc9ae5363145e3b2c71738045210cecf0b5f
Ezegrigolatto <ezegrigolatto@gmail.com>
2022-02-08 12:15:01 -0300

    first

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require('./src/app.js');
 
 // Syncing all the models at once.
 
-  server.listen(3001, () => {
+  server.listen(process.env.PORT, () => {
     console.log('listening at 3001'); // eslint-disable-line no-console
   });
 
