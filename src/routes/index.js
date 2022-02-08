c38532fd2c952973cb0eb56dec008cd877e39744
Ezegrigolatto <ezegrigolatto@gmail.com>
2022-02-08 16:20:45 -0300

    first

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,11 +9,21 @@ const upload = multer();
 const router = Router();
 
 router.post("/video", upload.single("file"), async (req, res) => {
-  // const arch = req.file;
-  // await pipeline(arch.stream, fs.createWriteStream(`${__dirname}/../uploads/${arch.originalName}`));
-  // await converter.convertToThumbnail(`${__dirname}/../uploads/${arch.originalName}`);
-  // const dir=fs.realpathSync(`${__dirname}/../../thumbnail/`) + "/tn.png";
-  res.send("ok");
+  try {
+    const arch = req.file;
+    await pipeline(
+      arch.stream,
+      fs.createWriteStream(`${__dirname}/../uploads/${arch.originalName}`)
+    );
+    await converter.convertToThumbnail(
+      `${__dirname}/../uploads/${arch.originalName}`
+    );
+    const dir = fs.realpathSync(`${__dirname}/../../thumbnail/`) + "/tn.png";
+    res.send("ok");
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
 });
 router.post("/thumbnail", function (req, res) {
   console.log(req.body);
